4a21040cd2559034da1f58d2ac75046221ccdb02
Efekan.Gvndk <76440360+efekangvndk@users.noreply.github.com>
2023-09-10 15:32:38 +0300

    Update: [Profile image save from firebase storage]

--- SocialMediaFirebase/ProfileViewController.swift
+++ SocialMediaFirebase/ProfileViewController.swift
@@ -30,5 +30,42 @@ class ProfileViewController: UIViewController , UIImagePickerControllerDelegate 
         picker.allowsEditing = true
         present(picker , animated: true)
     }
+    @IBAction func saveButton(_ sender: Any) {
+        // Burda da profile imagemizi kayıt etmek için tekrar auth kullanarak storage kayıt ediıyoruz.
+        
+        let storage = Storage.storage()
+        let storageReference2 = storage.reference()
+        
+        let ProfileFolderMedia = storageReference2.child("profile paper")
+        
+        if let data = profileImage.image?.jpegData(compressionQuality: 0.5){
+            
+            let profileImageRef = ProfileFolderMedia.child("profile jpeg")
+            profileImageRef.putData(data) { metadata, error in
+                if let error = error {
+                    // Hata durumunda işlemleri burada işleyebilirsiniz
+                    print("Hata: \(error.localizedDescription)")
+                } else {
+                    // Yükleme başarılı olduğunda işlemleri burada yapabilirsiniz
+                    print("Profil resmi başarıyla kaydedildi!")
+                }
+            }
+        } else {
+            // Eğer bir profil resmi seçilmemişse, uygun bir hata işlemi yapabilirsiniz
+            print("Lütfen bir profil resmi seçin.")
+        }
+            }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let selectedImage = info[.editedImage] as? UIImage{
+            profileImage.image = selectedImage
+        }
+        picker.dismiss(animated: true)
+        
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+                picker.dismiss(animated: true, completion: nil)
+            }
+    }
 
-}
+        }
+        
+    
